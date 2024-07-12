b57ce91e529eb8128e12cb34c250a75ffd731c8a
Guide5028 <guiderock55@gmail.com>
2024-07-12 10:13:53 +0700

    Initial commit

---
+++ Lab-2-compro-master/src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import EventCard from '@/components/StudentCard.vue'
+import Event from '@/types/Student'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/AnotherEventService'
+
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      console.log(response.data)
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
+</script>
+
+<template>
+  <div class="student">
+    <h1>Students Info</h1>
+    <!-- new element -->
+    <div class="events"></div>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+ 
+  .student {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
